5e8d72d2e0793cce61d4e9ecc063cb7e067e96f7
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-01-31 15:51:52 +0300

    optimize code to be minimize material-ui bundle size

---
+++ res-review/src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from '@material-ui/core';
+
+
+export default function Header() {
+	return (
+		<Button variant="contained" color="primary">
+			Hello World
+    	</Button>
+	);
+}
