56a3f1454dfe9765bab107d78879f89020363c82
Bill Kariri <billkariri@gmail.com>
2020-02-04 18:35:36 +0300

    remove material-ui code

--- res-review/src/App.js
+++ res-review/src/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import { /*CssBaseline*/ } from '@material-ui/core';
 import Header from './components/layout/Header';
 
 export default function App() {
-  return (
-    <React.Fragment>
-      {/* <CssBaseline> */}
-        <Header />
-      {/* </CssBaseline> */}
-    </React.Fragment>
-  )
+    return <Header />;
 }
-
