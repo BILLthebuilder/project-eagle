f75a74d81a317d90343eb3e08685ec4f71a3a441
Bill Kariri <billkariri@gmail.com>
2020-02-04 18:28:59 +0300

    remove some custom configs

--- res-review/config-overrides.js
+++ res-review/config-overrides.js
@@ -1,6 +1,6 @@
-/* config-overrides.js */
-const { useBabelRc, override } = require('customize-cra')
+// /* config-overrides.js */
+// const { useBabelRc, override } = require('customize-cra')
 
-module.exports = override(
-    useBabelRc()
-);
+// module.exports = override(
+//     useBabelRc()
+// );
