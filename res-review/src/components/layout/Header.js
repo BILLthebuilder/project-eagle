9ff182ec8fabda8b12b214fc026a833a7454467e
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-02-05 14:29:18 +0300

    antd experiments from official docs

--- res-review/src/components/layout/Header.js
+++ res-review/src/components/layout/Header.js
@@ -1,90 +1,27 @@
-import React, { useState } from 'react';
-import { Grommet, Box, Button, Heading, Collapsible, ResponsiveContext, Layer } from 'grommet';
-import { Notification, FormClose } from 'grommet-icons';
+import React from 'react';
+import ReactDom from 'react-dom';
+import { DatePicker, message } from 'antd';
+import '../../App.css';
 
-const theme = {
-    global: {
-        colors: {
-            brand: '#228BE6'
-        },
-        font: {
-            family: 'Roboto',
-            size: '18px',
-            height: '20px'
-        }
+export default class TestAntd extends React.Component {
+    state = {
+        date: null
+    };
+
+    handleChange = date => {
+        message.info(`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`);
+        this.setState({ date });
+    };
+
+    render() {
+        const { date } = this.state;
+        return (
+            <div style={{ width: 400, margin: '100px auto' }}>
+                <DatePicker onChange={this.handleChange} />
+                <div style={{ marginTop: 20 }}>
+                    Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+                </div>
+            </div>
+        );
     }
-};
-
-const AppBar = props => (
-    <Box
-        tag="header"
-        direction="row"
-        align="center"
-        justify="between"
-        background="brand"
-        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-        elevation="medium"
-        style={{ zIndex: '1' }}
-        {...props}
-    />
-);
-
-export default function Testgrommet() {
-    const [showSidebar, setShowSidebar] = useState(false);
-    return (
-        <Grommet theme={theme} /* themeMode="light" */ full>
-            <ResponsiveContext.Consumer>
-                {size => (
-                    <Box fill>
-                        <AppBar>
-                            <Heading level="3" margin="none">
-                                My App
-                            </Heading>
-                            <Button
-                                icon={<Notification />}
-                                onClick={() => setShowSidebar(!showSidebar)}
-                            />
-                        </AppBar>
-                        <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
-                            <Box flex align="center" justify="center">
-                                app body
-                            </Box>
-                            {!showSidebar || size !== 'small' ? (
-                                <Collapsible direction="horizontal" open={showSidebar}>
-                                    <Box
-                                        flex
-                                        width="medium"
-                                        background="light-2"
-                                        elevation="small"
-                                        align="center"
-                                        justify="center"
-                                    >
-                                        sidebar
-                                    </Box>
-                                </Collapsible>
-                            ) : (
-                                <Layer>
-                                    <Box
-                                        background="light-2"
-                                        tag="header"
-                                        justify="end"
-                                        align="center"
-                                        direction="row"
-                                    >
-                                        <Button
-                                            icon={<FormClose />}
-                                            onClick={() => setShowSidebar(false)}
-                                        />
-                                    </Box>
-                                    <Box fill background="light-2" align="center">
-                                        sidebar
-                                    </Box>
-                                </Layer>
-                            )}
-                        </Box>
-                    </Box>
-                )}
-            </ResponsiveContext.Consumer>
-        </Grommet>
-    );
 }
