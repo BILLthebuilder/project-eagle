e6ce4bccb511d78e126da7c3256bdeb5a40a437d
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-02-05 14:29:18 +0300

    antd experiments from official docs

--- res-review/src/App.js
+++ res-review/src/App.js
@@ -2,5 +2,9 @@ import React from 'react';
 import Header from './components/layout/Header';
 
 export default function App() {
-    return <Header />;
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
 }
