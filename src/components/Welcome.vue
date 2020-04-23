6274e8c209232162923d0ef781d18d7b3a409086
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-04-23 16:53:54 +0300

    basic markup structure for the welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="big"></div>
+        <div class="small"></div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/welcome.scss';
+</style>
