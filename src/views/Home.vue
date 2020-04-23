85f32f62f056e9cb193f7b00f48cc6212be585af
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-04-23 16:53:23 +0300

    add the welcome component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
         <Navbar />
+        <Welcome />
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar';
+import Welcome from '@/components/Welcome';
 
 export default {
     name: 'Home',
     components: {
-        Navbar
+        Navbar,
+        Welcome
     }
 };
 </script>
